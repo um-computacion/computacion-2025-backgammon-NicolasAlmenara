c785764b6651188f903c952840c50a7905808801
Nicolas Almenara <n.almenara@alumno.um.edu.ar>
2025-09-04 15:40:49 -0300

    se agrega el constructor de la clase checker

--- core/checker.py
+++ core/checker.py
@@ -1,3 +1,12 @@
 class Checker:
     bar_counts = {"white": 0, "black": 0}
     off_counts = {"white": 0, "black": 0}
+    def __init__(self, color, estado="tablero"):
+        self.color = color                  
+        self.estado = "tablero"             
+        if estado == "barra":
+            Checker.bar_counts[self.color] += 1
+            self.estado = "barra"
+        elif estado == "fuera":
+            Checker.off_counts[self.color] += 1
+            self.estado = "fuera"
