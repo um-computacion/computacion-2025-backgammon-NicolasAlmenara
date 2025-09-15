379ee22ce162d137baeab4b8d13b6233e07d4519
Nicolas Almenara <n.almenara@alumno.um.edu.ar>
2025-09-15 00:26:02 -0300

    se agregan los test que faltaban de la clase player

--- tests/test_player.py
+++ tests/test_player.py
@@ -16,3 +16,16 @@ class TestPlayer(unittest.TestCase):
         p2 = Player("Ana", "black")
         self.assertNotEqual(p1.name, p2.name)
         self.assertNotEqual(p1.color, p2.color)
+
+    def test_change_name(self):
+        p = Player("Juan", "white")
+        p.name = "Pedro"
+        self.assertEqual(p.name, "Pedro")
+
+    def test_change_color(self):
+        p = Player("Ana", "black")
+        p.color = "white"
+        self.assertEqual(p.color, "white")
+
+if __name__ == "__main__":
+    unittest.main()
