576078b4e1d517c44afec15f401167d9cd6634f9
Nicolas Almenara <n.almenara@alumno.um.edu.ar>
2025-10-02 17:59:38 -0300

    se agregan test para la clase board

--- tests/test_board.py
+++ tests/test_board.py
@@ -47,5 +47,56 @@ class TestBoard(unittest.TestCase):
             point = self.board.get_point(point_num)
             self.assertEqual(point[0], "")
             self.assertEqual(point[1], 0)
+    def test_get_point_valid(self):
+        """Verifica que se pueden obtener puntos válidos"""
+        point1 = self.board.get_point(1)
+        self.assertIsNotNone(point1)
+        self.assertIsInstance(point1, list)
+        self.assertEqual(len(point1), 2)
+        point24 = self.board.get_point(24)
+        self.assertIsNotNone(point24)
+        self.assertIsInstance(point24, list)
+    def test_get_point_invalid(self):
+        """Verifica el manejo de puntos inválidos"""
+        self.assertIsNone(self.board.get_point(0))
+        self.assertIsNone(self.board.get_point(25))
+        self.assertIsNone(self.board.get_point(-1))
+        self.assertIsNone(self.board.get_point(100))
+    def test_move_checker_normal(self):
+        """Verifica el movimiento normal de una ficha"""
+        self.board.move_checker(1, 2, "white")
+        point1 = self.board.get_point(1)
+        self.assertEqual(point1[1], 1)
+        point2 = self.board.get_point(2)
+        self.assertEqual(point2[0], "white")
+        self.assertEqual(point2[1], 1)
+    def test_move_checker_to_occupied_same_color(self):
+        """Verifica mover a un punto ocupado por el mismo color"""
+        self.board.move_checker(1, 12, "white")
+        point12 = self.board.get_point(12)
+        self.assertEqual(point12[0], "white")
+        self.assertEqual(point12[1], 6)
+    def test_move_checker_capture(self):
+        """Verifica la captura de una ficha enemiga"""
+        self.board.points[1] = ["black", 1]
+        self.board.move_checker(1, 2, "white")
+        self.assertEqual(self.board.bar["black"], 1)
+        point2 = self.board.get_point(2)
+        self.assertEqual(point2[0], "white")
+        self.assertEqual(point2[1], 1)
+    def test_move_checker_from_bar(self):
+        """Verifica el movimiento desde la barra"""
+        self.board.bar["white"] = 1
+        self.board.move_checker(25, 2, "white")
+        self.assertEqual(self.board.bar["white"], 0)
+        point2 = self.board.get_point(2)
+        self.assertEqual(point2[0], "white")
+        self.assertEqual(point2[1], 1)
+    def test_move_checker_bear_off(self):
+        """Verifica sacar una ficha del tablero"""
+        self.board.move_checker(1, 0, "white")
+        point1 = self.board.get_point(1)
+        self.assertEqual(point1[1], 1)
+        self.assertEqual(self.board.off["white"], 1)
 if __name__ == "__main__":
     unittest.main()
