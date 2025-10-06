6d5744983e23abbe4012d43335f7063d542415b5
Nicolas Almenara <n.almenara@alumno.um.edu.ar>
2025-10-05 22:17:22 -0300

    se agregan los test para la clase dice y se modifica la clase player

--- core/player.py
+++ core/player.py
@@ -1,7 +1,12 @@
 class Player:
+    """Un jugador del juego"""
     def __init__(self, name, color):
-        self.name = name      
-        self.color = color    
-
-    def __str__(self):
-        return f"{self.name} ({self.color})"
+        """Crea un jugador con nombre y color"""
+        self.__name__ = name
+        self.__color__ = color
+    def get_name(self):
+        """Obtiene el nombre del jugador"""
+        return self.__name__
+    def get_color(self):
+        """Obtiene el color del jugador"""
+        return self.__color__
